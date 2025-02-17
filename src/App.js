1621d0c395e4dc328a32b78e84412dc5f1835a93
AurelienLELEU <aurelien.leleu78@gmail.com>
2025-02-17 16:01:45 +0100

    lighter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
